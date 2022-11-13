2ff2bdc904517eb8b62221dd97c6b30f458cc465
Eugene Stsefankou <stefjen@mail.ru>
2022-11-13 23:14:18 +0300

    Initial Commit

---
+++ PineComm/PineCommApp.swift
@@ -0,0 +1,17 @@
+//
+//  PineCommApp.swift
+//  PineComm
+//
+//  Created by Евгений on 13.11.22.
+//
+
+import SwiftUI
+
+@main
+struct PineCommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
